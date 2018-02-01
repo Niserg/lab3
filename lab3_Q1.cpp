c90f3218be2c545fc6e33c0f305fbd69f05b9722
Niserg <31318028+Niserg@users.noreply.github.com>
2018-02-01 15:14:31 +0530

    Update lab3_Q1.cpp

--- lab3_Q1.cpp
+++ lab3_Q1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class node
-{
+{                                                           // list has to be doubly linked
  public:
  int datum;
  node * next, * prev;
@@ -11,7 +11,7 @@ class node
  node();
 };
  
- node :: node()
+ node :: node()                                           // constructor overloading to allow initialization in different ways
  {
 	next = prev = NULL;
  }
@@ -43,7 +43,7 @@ class doublyLinkedList
  void doublyLinkedList :: insert( int datum)
  {	
 	node * temp = new node(datum);
-  	if(head == NULL)
+  	if(head == NULL)                                            //if there is no node in list
 	    head = tail = temp;
 	else
 	{	
@@ -58,13 +58,13 @@ class doublyLinkedList
  void doublyLinkedList :: insertAt( int datum, int pos)
  {
 	node * temp = new node(datum);
-	node * check = head;
+	node * check = head;                              // check will help to reach at position specified
 	int count = 1;
 	while(check != NULL)
         {
 	  check = check->next;
 	  count++;
-          if(count == pos-1) break;
+          if(count == pos-1) break;                        // stop before the place of node insertion
 	}
 	if(count < pos-1)
 	{
@@ -75,8 +75,9 @@ class doublyLinkedList
 	{
 	 temp->prev = check;
 	 temp->next = check->next;
-	 if(temp->next != NULL) temp->next->prev = temp;
- 	 if(check != NULL) check->next = temp;
+	 if(temp->next != NULL) temp->next->prev = temp;          // to avoid the case when check is the last node
+ 	 if(check != NULL) check->next = temp;                   /* to avoid the case when there is only one element head and
+		                                                  we want insertion at 2nd position*/
 	}	
  }
 
@@ -96,7 +97,7 @@ class doublyLinkedList
  {
  	int count = 1;
 	node * check = head;
-	while(check != NULL)
+	while(check != NULL)                                // to reach one node before the node to be deleted
 	{
 	 check = check->next;
 	 count++;
@@ -105,8 +106,9 @@ class doublyLinkedList
 	if(count < pos)	cout << "\nThe list has only "<< count << " number of elements. There is no node at the specified position.\n";
 	else
 	{
-	 check->prev->next = check->next;
-	 check->next->prev = check->prev;
+	 if(check != NULL) check->prev->next = check->next;                           // to avoid the case when check is the only element
+		else head = NULL;
+	 if(check->next != NULL) check->next->prev = check->prev;         // to avoid the case when it is the last element that is to be deleted
 	}
 	
  }
